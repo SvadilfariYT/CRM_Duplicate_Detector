0b78d0e9082bdb4ce43668542afd9ed1945ae25f
SvadilfariYT <dominik.sekular@gmail.com>
2023-05-17 21:42:49 +0200

    load all csv files of dir

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
+import pandas as pd
+import glob
+
+def process_duplicate_entries():
+    dataframes = load_csv_files('data/ID-Leads')
+
+def load_csv_files(directory):
+    # Die Dateien mit der Endung .csv im Verzeichnis finden
+    files = glob.glob(directory + "/*.csv")
+    
+    dataframes = []
+    
+    # Jede gefundene Datei laden
+    for file in files:
+        
+        df = pd.read_csv(file, encoding='utf-16', sep='\t')
+        dataframes.append(df)
+        print(df.columns)
+    
+    return dataframes
+
 if __name__ == '__main__':
-    a = 0
+    process_duplicate_entries()
