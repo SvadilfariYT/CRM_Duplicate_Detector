da14e8382b71ef5a8a6dff59e6f17c30b5dafe81
SvadilfariYT <dominik.sekular@gmail.com>
2023-05-17 21:51:28 +0200

    combine dataframes

--- main.py
+++ main.py
@@ -4,13 +4,22 @@ import glob
 def process_duplicate_entries():
     dataframes = load_csv_files('data/ID-Leads')
 
+    combined_df = combine_dataframes(dataframes)
+
+def combine_dataframes(dataframes):
+    combined_df = pd.concat(dataframes, ignore_index=True)
+    
+    print(combined_df.shape)
+    print(combined_df.head(10))
+
+    return combined_df
+
+
 def load_csv_files(directory):
-    # Die Dateien mit der Endung .csv im Verzeichnis finden
     files = glob.glob(directory + "/*.csv")
     
     dataframes = []
     
-    # Jede gefundene Datei laden
     for file in files:
         
         df = pd.read_csv(file, encoding='utf-16', sep='\t')
